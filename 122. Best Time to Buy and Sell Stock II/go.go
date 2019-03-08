7652de05dc6e3e5a999ff53a4f134eb3c0d4c405
kunpengdai <kunpengdai@foxmail.com>
2019-03-08 14:09:47 +0800

    bset time to buy ans sell stock II

---
+++ 122. Best Time to Buy and Sell Stock II/go.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
+}
+func maxProfit(prices []int) int {
+	res := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i]-prices[i-1] > 0 {
+			res += (prices[i] - prices[i-1])
+		}
+	}
+	return res
+}

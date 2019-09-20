3bf127682ac03ba5fbd8d9113e4fed193a5a143f
代坤鹏 <daikunepng@douyu.tv>
2019-09-20 16:31:55 +0800

    [80] Remove Duplicates from Sorted Array II

---
+++ 80.remove-duplicates-from-sorted-array-ii.go
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode id=80 lang=golang
+ *
+ * [80] Remove Duplicates from Sorted Array II
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/
+ *
+ * algorithms
+ * Medium (40.90%)
+ * Likes:    760
+ * Dislikes: 581
+ * Total Accepted:    219.4K
+ * Total Submissions: 530.4K
+ * Testcase Example:  '[1,1,1,2,2,3]'
+ *
+ * Given a sorted array nums, remove the duplicates in-place such that
+ * duplicates appeared at most twice and return the new length.
+ * 
+ * Do not allocate extra space for another array, you must do this by modifying
+ * the input array in-place with O(1) extra memory.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Given nums = [1,1,1,2,2,3],
+ * 
+ * Your function should return length = 5, with the first five elements of nums
+ * being 1, 1, 2, 2 and 3 respectively.
+ * 
+ * It doesn't matter what you leave beyond the returned length.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Given nums = [0,0,1,1,1,1,2,3,3],
+ * 
+ * Your function should return length = 7, with the first seven elements of
+ * nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
+ * 
+ * It doesn't matter what values are set beyond the returned length.
+ * 
+ * 
+ * Clarification:
+ * 
+ * Confused why the returned value is an integer but your answer is an array?
+ * 
+ * Note that the input array is passed in by reference, which means
+ * modification to the input array will be known to the caller as well.
+ * 
+ * Internally you can think of this:
+ * 
+ * 
+ * // nums is passed in by reference. (i.e., without making a copy)
+ * int len = removeDuplicates(nums);
+ * 
+ * // any modification to nums in your function would be known by the caller.
+ * // using the length returned by your function, it prints the first len
+ * elements.
+ * for (int i = 0; i < len; i++) {
+ * print(nums[i]);
+ * }
+ * 
+ * 
+ */
+func removeDuplicates(nums []int) int {
+	s,f := 0,1
+	showOnce := false
+	for f < len(nums){
+		if nums[f]==nums[s]{
+			if !showOnce {
+				nums[s+1],nums[f] = nums[f],nums[s+1]
+				s++
+				f++
+				showOnce = true
+				continue
+			}else{
+				f++
+			}
+		}else{
+			showOnce = false
+			nums[s+1],nums[f] = nums[f],nums[s+1]
+			s++
+			f++
+		}
+	}
+	return s+1
+}
+

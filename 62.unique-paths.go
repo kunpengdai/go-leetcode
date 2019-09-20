773c0a52c035c0cc10731320b9ae2068dd1ec8b9
代坤鹏 <daikunepng@douyu.tv>
2019-09-20 16:31:16 +0800

    [62] Unique Paths

---
+++ 62.unique-paths.go
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=62 lang=golang
+ *
+ * [62] Unique Paths
+ *
+ * https://leetcode.com/problems/unique-paths/description/
+ *
+ * algorithms
+ * Medium (48.45%)
+ * Likes:    1887
+ * Dislikes: 133
+ * Total Accepted:    331.8K
+ * Total Submissions: 674.4K
+ * Testcase Example:  '3\n2'
+ *
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in
+ * the diagram below).
+ * 
+ * The robot can only move either down or right at any point in time. The robot
+ * is trying to reach the bottom-right corner of the grid (marked 'Finish' in
+ * the diagram below).
+ * 
+ * How many possible unique paths are there?
+ * 
+ * 
+ * Above is a 7 x 3 grid. How many possible unique paths are there?
+ * 
+ * Note: m and n will be at most 100.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: m = 3, n = 2
+ * Output: 3
+ * Explanation:
+ * From the top-left corner, there are a total of 3 ways to reach the
+ * bottom-right corner:
+ * 1. Right -> Right -> Down
+ * 2. Right -> Down -> Right
+ * 3. Down -> Right -> Right
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: m = 7, n = 3
+ * Output: 28
+ * 
+ */
+func uniquePaths(m int, n int) int {
+	dp := make([][]int,m)
+	for i := range dp{
+		dp[i] = make([]int,n)
+	}
+	for i :=0;i<m;i++{
+		dp[i][0] = 1
+	}
+	for i :=0;i<n;i++{
+		dp[0][i] = 1
+	}
+	for i :=1;i<m;i++{
+		for j:=1;j<n;j++{
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[m-1][n-1]
+}
+

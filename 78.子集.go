7eed34ed4dbd3c8bbd595b9d9e3513d3d74627cf
kunpeng.dai01 <kunpeng.dai01@liulishuo.com>
2020-09-24 14:13:58 +0800

    78. subsets bits and dfs

--- 78.子集.go
+++ 78.子集.go
@@ -22,5 +22,20 @@ func dfs(nums []int, pos int, res *[][]int, tmpSlice []int) {
 	dfs(nums, pos+1, res, tmpSlice)
 }
 
+func subsets1(nums []int) [][]int {
+	resCnt := 1 << uint64(len(nums))
+	res := make([][]int, resCnt)
+	for i := 0; i < resCnt; i++ {
+		sl := make([]int, 0)
+		for j := 0; j < len(nums); j++ {
+			if i&(1<<uint64(j)) != 0 {
+				sl = append(sl, nums[j])
+			}
+		}
+		res[i] = sl
+	}
+
+	return res
+}
 // @lc code=end
 

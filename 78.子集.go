daf3f62c5281ca2025b84063889285407f0812f3
kunpeng.dai01 <kunpeng.dai01@liulishuo.com>
2020-09-24 11:11:43 +0800

    78. subsets dfs

--- 78.子集.go
+++ 78.子集.go
@@ -6,19 +6,20 @@
 
 // @lc code=start
 func subsets(nums []int) [][]int {
-	resCnt := 1 << uint64(len(nums))
-	res := make([][]int, resCnt)
-	for i := 0; i < resCnt; i++ {
-		sl := make([]int, 0)
-		for j := 0; j < len(nums); j++ {
-			if i&(1<<uint64(j)) != 0 {
-				sl = append(sl, nums[j])
-			}
-		}
-		res[i] = sl
+	res := make([][]int, 0, 1<<uint64(len(nums)))
+	dfs(nums, 0, &res, []int{})
+	return res
+}
+
+func dfs(nums []int, pos int, res *[][]int, tmpSlice []int) {
+	if pos == len(nums) {
+		*res = append(*res, append([]int{}, tmpSlice...))
+		return
 	}
 
-	return res
+	dfs(nums, pos+1, res, tmpSlice)
+	tmpSlice = append(tmpSlice, nums[pos])
+	dfs(nums, pos+1, res, tmpSlice)
 }
 
 // @lc code=end

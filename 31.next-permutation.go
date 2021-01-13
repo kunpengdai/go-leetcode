50a5c3be529ad98addcdc55dab485405358ebdac
kunpeng dai <kunpengdai@foxmail.com>
2021-01-13 15:53:22 +0800

    31. next permustaion

--- 31.next-permutation.go
+++ 31.next-permutation.go
@@ -15,39 +15,39 @@
  *
  * Implement next permutation, which rearranges numbers into the
  * lexicographically next greater permutation of numbers.
- * 
+ *
  * If such arrangement is not possible, it must rearrange it as the lowest
  * possible order (ie, sorted in ascending order).
- * 
+ *
  * The replacement must be in-place and use only constant extra memory.
- * 
+ *
  * Here are some examples. Inputs are in the left-hand column and its
  * corresponding outputs are in the right-hand column.
- * 
+ *
  * 1,2,3 → 1,3,2
  * 3,2,1 → 1,2,3
  * 1,1,5 → 1,5,1
- * 
+ *
  */
-func nextPermutation(nums []int)  {
-	pos,rep := -1,0
-    for i:=len(nums)-2;i >= 0;i--{
-		if nums[i]< nums[i+1]{
+func nextPermutation(nums []int) {
+	pos, rep := -1, 0
+	for i := len(nums) - 2; i >= 0; i-- {
+		if nums[i] < nums[i+1] {
 			pos = i
 			break
 		}
 	}
-	if pos == -1{
+	if pos == -1 {
 		sort.Ints(nums)
 		return
 	}
-	for i:=len(nums)-1;i>pos;i--{
-		if nums[i] >nums[pos]{
+	for i := len(nums) - 1; i > pos; i-- {
+		if nums[i] > nums[pos] {
 			rep = i
 			break
 		}
 	}
-	nums[pos],nums[rep] = nums[rep],nums[pos]
+	nums[pos], nums[rep] = nums[rep], nums[pos]
 	sort.Ints(nums[pos+1:])
 }
 

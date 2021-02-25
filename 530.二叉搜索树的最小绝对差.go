dbbc1369f3a641ae96783a24a0550e2b7e26772a
kunpeng dai <kunpengdai@foxmail.com>
2021-02-25 18:17:39 +0800

    530. 二叉搜索树的最小绝对差

---
+++ 530.二叉搜索树的最小绝对差.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=530 lang=golang
+ *
+ * [530] 二叉搜索树的最小绝对差
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func getMinimumDifference(root *TreeNode) int {
+    ans, pre := math.MaxInt64, -1
+    var dfs func(*TreeNode)
+    dfs = func(node *TreeNode) {
+        if node == nil {
+            return
+        }
+        dfs(node.Left)
+        if pre != -1 && node.Val-pre < ans {
+            ans = node.Val - pre
+        }
+        pre = node.Val
+        dfs(node.Right)
+    }
+    dfs(root)
+    return ans
+}
+// @lc code=end
+
